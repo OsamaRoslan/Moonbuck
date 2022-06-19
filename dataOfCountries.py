3a0c78fbc52cc41e09eda8db7d9d96def27a1cc0
OsamaRoslan <draganosama@gmail.com>
2022-06-19 15:49:28 +0800

    meh

---
+++ dataOfCountries.py
@@ -0,0 +1,26 @@
+class Shop:
+    latitud = 0
+    longitud = 0
+    graph = []
+
+    def __init__(self, latitud, longitud):
+        self.latitud = latitud
+        self.longitud = longitud
+
+
+
+# This class contains the name of the country and some shops in different location
+class Countries:
+    name = "Name of Contry"
+    shops = []
+    grapgh = []
+
+    def __init__(self, country):
+        self.name = country
+
+    def listOfShops(self):
+        pass
+
+    def getGraph (self):
+        pass
+
